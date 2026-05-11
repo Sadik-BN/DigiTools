de69ff9cf16ee6be3cbe40c2e1079d7c25ba9735
Sadik-BN <sadikonline4@gmail.com>
2026-05-11 22:46:12 +0600

    Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <Navbar>
+
+        </Navbar>
+      </header>
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
